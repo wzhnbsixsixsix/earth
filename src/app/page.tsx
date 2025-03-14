07f04a12dd0d45f2d349cec941ceec52a2273d91
StickyDogShit <w2hnb666@163.com>
2025-03-14 23:11:17 +0000

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Globe } from "@/components/magicui/globe";
+
+export default function Home() {
+  return (
+    <div className="relative flex min-h-screen w-full items-center justify-center">
+      <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg border bg-background px-40 pb-40 pt-8 md:pb-60">
+        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+          SusCampus
+        </span>
+        <Globe className="top-28" />
+        <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
+      </div>
+    </div>
+  );
+}
